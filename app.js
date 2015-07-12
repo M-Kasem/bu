3af6661b2a78036300ae4d6fcfcc1b85e7efa319
MohamedSamy <Mohamed.Samy.K@gmail.com>
2015-07-12 07:33:01 +0200

    Revision

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ var metaInfos = angular.module('metaInfos', []);
 
 metaInfos.controller('UserCtrl', ['$scope', function($scope){
     $scope.names = ['Mohamed', 'Kareem', 'Ahmed'];
+    $scope.toggles = [true, true, true];
     $scope.name = 'Anonymous';
     $scope.addName = function(name){
         $scope.names.push(name);
+    }
+    $scope.updateName = function(index, name){
+        $scope.names[index] = name;
+        $scope.toggleName(index);
+    }
+    $scope.toggleName = function(name){
+        $scope.toggles[name] = !$scope.toggles[name];
     }
 }]);
